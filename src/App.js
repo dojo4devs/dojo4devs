572419a6a28e9e14a975a3331f23f6d6b54605f4
Carolinne Santos Rodrigues <carolinne.rodrigues7@hotmail.com>
2020-06-20 19:05:56 -0300

    Adds initial header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './dojo4devs.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Header from './pages/header/Header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header/>
+        <img src={logo} />
       </header>
     </div>
   );
