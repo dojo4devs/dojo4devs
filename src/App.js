63f331e30c61ba11560f253918663f27ec40a6af
Carolinne Santos Rodrigues <carolinne.rodrigues7@hotmail.com>
2020-06-21 23:59:38 -0300

    Landing page base

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
-import React from 'react';
-import logo from './dojo4devs.png';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Header from './pages/header/Header';
+import React, { useEffect, Fragment } from "react";
+import AOS from "aos";
+import $ from "jquery";
 
-function App() {
+import Header from "./components/Header";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+import "aos/dist/aos.css";
+import "./assets/styles/main.scss";
+
+const App = () => {
+  useEffect(() => {
+    AOS.init({ once: true });
+
+    let navElement = $("nav");
+
+    $(function() {
+      $(window).scrollTop() > navElement.innerHeight()
+        ? navElement.addClass("sticky")
+        : navElement.removeClass("sticky");
+    });
+    $(window).on("scroll", function() {
+      $(window).scrollTop() > navElement.innerHeight()
+        ? navElement.addClass("sticky")
+        : navElement.removeClass("sticky");
+    });
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <Header/>
-        <img src={logo} />
-      </header>
-    </div>
+    <Fragment>
+      <Header />
+      <main>
+        <Home />
+        <About />
+        <Contact />
+      </main>
+    </Fragment>
   );
-}
+};
 
 export default App;
